04332cd76814980733f2c00e7964bb3f45ddbec9
Henrik Alfke <argonavis80@users.noreply.github.com>
2021-07-08 14:32:07 +0200

    Setup initial version (#1)
    * Add sample project, readme.md and test data.
    
    * Reset SDK nuget package version to 0.1.0.
    
    Co-authored-by: Philipp Ranft <philipp.ranft@navis.com>

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # MACS3.SDK.Sample
-Example project to use MACS3 SDK for stability, stress and lashing calculations and DG segregation checks.
+Example project how to use MACS3 SDK for stability and stress calculations and DG segregation checks.
+
+### Heads UP! - Number 1
+This sample requires the NuGet package `Macs3.Sdk`. This package is currently not available via a public NuGet-Feed and hence the package restore operation will fail. Please reach out to api.macs3@navis.com to get the NuGet package manually.
+
+It is recommended to use a local feed to make the package available for the restore process. Please find details to set up a local source [here](https://docs.microsoft.com/en-us/nuget/consume-packages/install-use-packages-visual-studio#package-sources).
+
+### Heads UP! - Number 2
+In order to use the SDK, you need a product key to enable a license and to fetch vessel profile descriptions to perform the calculations and checks. Reach out to api.macs3@navis.com to get started.
+
+## Prerequisites
+- Microsoft .NET Framework 4.8
+- Visual C++ 2019 redistributable
+- Make sure the package `Macs3.Sdk` is available via a local package source.
+- Make sure you have a valid product key to activate the SDK.
+
+## Get started
+1. Clone this repository.
+1. Open a terminal and navigate to `Src\Macs3.Sdk.Sample`.
+1. Build the project with `dotnet build`.
+1. Start the program `Src\Macs3.Sdk.Sample\bin\Debug\net48\win-x64\Macs3.Sdk.Licensing.exe` to activate your license using the product key. During the registration process you have to provide some details.
+1. Run the sample with `dotnet run --no-build`.
---
+++ Src/Macs3.Sdk.Sample/Program.cs
@@ -0,0 +1,75 @@
+﻿using Macs3.Calculations.DaGo.Abstractions.Models.DaGo;
+using Macs3.Calculations.Stability.Abstractions.Models.Calculations;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Macs3.Sdk.Sample
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            var source = new CancellationTokenSource();
+            var token = source.Token;
+
+            await PerformStabilityCalculationAsync(token);
+            await PerformDaGoCheckAsync(token);
+
+            Console.Write("Press any key to exit... ");
+            Console.ReadKey();
+        }
+
+        private static async Task PerformStabilityCalculationAsync(CancellationToken token)
+        {
+            /* Create an instance of Stability & Stress Calculation service component.
+             * This is a fast and lightweight operation that should not fail. */
+            var service = new StabilityStressService();
+
+            /* The method InitializeAsync must be called before any calculations can be performed.
+             * This method will download the profile from the License Portal. Hence, the operation
+             * is slow and may fail if the profile is not available, not configured or when the
+             * license is not activated. */
+            await service.InitializeAsync(9348089, token: token);
+
+            /* The request parameters of the calculation to be performed. In this example, the request
+             * parameters are loaded from a JSON file. */
+            var parameters =
+                JsonConvert.DeserializeObject<CalculationsParameter>(File.ReadAllText(@"TestData\StabilityCalc_9348089.json"));
+
+            // Get the calculation parameters for the vessel and perform a calculation.
+            var result1 = await service.Parameters();
+            var result2 = await service.Calculate(parameters);
+
+            // Write the results on the console.
+            Console.WriteLine(JsonConvert.SerializeObject(result1, Formatting.Indented));
+            Console.WriteLine(JsonConvert.SerializeObject(result2, Formatting.Indented));
+        }
+
+        private static async Task PerformDaGoCheckAsync(CancellationToken token)
+        {
+            /* Create an instance of DaGo Calculation service component.
+             * This is a fast and lightweight operation that should not fail. */
+            var service = new DaGoService();
+
+            /* The method InitializeAsync must be called before any checks can be performed.
+             * This method will download the profile from the License Portal. Hence, the operation
+             * is slow and may fail if the profile is not available, not configured or when the
+             * license is not activated. */
+            await service.InitializeAsync(9348089, token: token);
+
+            /* The request parameters of the check to be performed. In this example, the request
+             * parameters are loaded from a JSON file. */
+            var parameters =
+                JsonConvert.DeserializeObject<DaGoCheckParameters>(File.ReadAllText(@"TestData\DaGoCheck_9348089.json"));
+
+            // Perform the check.
+            var result = await service.DaGoCheckResult(parameters);
+
+            // Write the results on the console.
+            Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
+        }
+    }
+}
